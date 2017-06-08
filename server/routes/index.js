11e4efe65ad23734927f0cfa434513d83d639e6d
arahito <uarahito@yahoo.com>
2017-06-08 16:02:37 +0700

    add fitur CRUD, signin, and signup user

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+let userController = require('../controllers/userController')
+let helpers = require('../helpers/helpers')
+
+// NOTE: user
+router.post('/signup', userController.signUp)
+router.post('/signin', userController.signIn)
+router.post('/signinfb', userController.signInFB)
+router.get('/allusers', userController.getAllUsers)
+router.get('/detailuser/:id', userController.getUserById)
+router.put('/edituser/:id', userController.editUser)  // password harus disertakan
+router.delete('/deleteuser/:id', userController.deleteUserById)
+
+module.exports = router
